bba206eef27f7dd5ef5f09041e444070472052a4
Esker <kyz173@naver.com>
2020-01-31 17:06:25 +0900

    HSV

---
+++ Esker/PersonalTest/HSV.py
@@ -0,0 +1,11 @@
+import cv2
+
+src = cv2.imread("pablo.jpg", cv2.IMREAD_COLOR)
+hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
+h, s, v = cv2.split(hsv)
+
+cv2.imshow("h", h)
+cv2.imshow("s", s)
+cv2.imshow("v", v)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
