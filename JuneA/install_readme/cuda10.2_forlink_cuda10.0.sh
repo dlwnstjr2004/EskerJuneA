6d08ea18d8357c41489f069a082d4a48b6f1a701
JuneA <dlwnstjr2004@naver.com>
2020-03-12 12:15:55 +0900

    정리

--- JuneA/install_readme/cuda10.2_forlink_cuda10.0.sh
+++ JuneA/install_readme/cuda10.2_forlink_cuda10.0.sh
@@ -1,8 +1,8 @@
 sudo ln -s /usr/local/cuda-10.2/lib64/libcudart.so.10.2 /usr/local/cuda-10.2/lib64/libcudart.so.10.0
 sudo ln -s /usr/local/cuda-10.2/lib64/libcufft.so.10 /usr/local/cuda-10.2/lib64/libcufft.so.10.0
 sudo ln -s /usr/local/cuda-10.2/lib64/libcurand.so.10 /usr/local/cuda-10.2/lib64/libcurand.so.10.0
-sudo ln -s /usr/local/cuda-10.2/lib64/libcusolver.so.10 /usr/local/cuda-10.2/lib64/cusolver.so.10.0
-sudo ln -s /usr/local/cuda-10.2/lib64/libcusparse.so.10 /usr/local/cuda-10.2/lib64/libcusparse.10.0
+sudo ln -s /usr/local/cuda-10.2/lib64/libcusolver.so.10 /usr/local/cuda-10.2/lib64/libcusolver.so.10.0
+sudo ln -s /usr/local/cuda-10.2/lib64/libcusparse.so.10 /usr/local/cuda-10.2/lib64/libcusparse.so.10.0
 sudo ln -s /usr/lib/x86_64-linux-gnu/libcublas.so.10 /usr/local/cuda-10.2/lib64/libcublas.so.10.0
-sudo ln -s /home/junea/lib/TensorRT-6.0.1.8/lib/libnvifer.so.6 /home/junea/lib/TensorRT-6.0.1.8/lib/libnvinfer.so.5
-sudo ln -s /home/junea/lib/TensorRT-6.0.1.8/lib/libnvifer_plugin.so.6 /home/junea/lib/TensorRT-6.0.1.8/lib/libnvinfer_plugin.so.5
+sudo ln -s /home/junea/lib/TensorRT-6.0.1.8/lib/libnvinfer.so.6 /home/junea/lib/TensorRT-6.0.1.8/lib/libnvinfer.so.5
+sudo ln -s /home/junea/lib/TensorRT-6.0.1.8/lib/libnvinfer_plugin.so.6 /home/junea/lib/TensorRT-6.0.1.8/lib/libnvinfer_plugin.so.5
