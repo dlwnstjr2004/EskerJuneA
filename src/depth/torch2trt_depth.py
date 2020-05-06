50780d48ecdcb6597c7901f13801ea40fe8cddba
JuneA <dlwnstjr2004@naver.com>
2020-05-06 20:50:18 +0900

    pass

---
+++ src/depth/torch2trt_depth.py
@@ -0,0 +1,205 @@
+# Copyright Niantic 2019. Patent Pending. All rights reserved.
+#
+# This software is licensed under the terms of the Monodepth2 licence
+# which allows for non-commercial use only, the full terms of which are made
+# available in the LICENSE file.
+
+from __future__ import absolute_import, division, print_function
+
+import os
+import sys
+import glob
+import argparse
+import numpy as np
+import PIL.Image as pil
+import matplotlib as mpl
+import matplotlib.cm as cm
+
+import torch
+from torchvision import transforms, datasets
+from torchsummary import summary
+from torch2trt import torch2trt
+
+import networks
+from layers import disp_to_depth
+from utils import download_model_if_doesnt_exist
+import cv2
+import time
+
+def open_cam_usb(dev, width, height):
+    # We want to set width and height here, otherwise we could just do:
+    #     return cv2.VideoCapture(dev)
+    gst_str = ('v4l2src device=/dev/video{} ! '
+               'video/x-raw, width=(int){}, height=(int){} ! '
+               'videoconvert ! appsink').format(dev, width, height)
+    return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Simple testing funtion for Monodepthv2 models.')
+
+    parser.add_argument('--video_path', type=str,
+                        help='path to a test video', required=True)
+    parser.add_argument('--model_name', type=str,
+                        help='name of a pretrained model to use',
+                        choices=[
+                            "mono_640x192",
+                            "stereo_640x192",
+                            "mono+stereo_640x192",
+                            "mono_no_pt_640x192",
+                            "stereo_no_pt_640x192",
+                            "mono+stereo_no_pt_640x192",
+                            "mono_1024x320",
+                            "stereo_1024x320",
+                            "mono+stereo_1024x320"])
+    parser.add_argument('--ext', type=str,
+                        help='image extension to search for in folder', default="jpg")
+    parser.add_argument("--no_cuda",
+                        help='if set, disables CUDA',
+                        action='store_true')
+
+    return parser.parse_args()
+
+
+def test_simple(args):
+    """Function to predict for a single image or folder of images
+    """
+    assert args.model_name is not None, \
+        "You must specify the --model_name parameter; see README.md for an example"
+
+    if torch.cuda.is_available() and not args.no_cuda:
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
+    download_model_if_doesnt_exist(args.model_name)
+    model_path = os.path.join("models", args.model_name)
+    print("-> Loading model from ", model_path)
+    encoder_path = os.path.join(model_path, "encoder.pth")
+    depth_decoder_path = os.path.join(model_path, "depth.pth")
+
+    # LOADING PRETRAINED MODEL
+    print("   Loading pretrained encoder")
+    encoder = networks.ResnetEncoder(18, False)
+    loaded_dict_enc = torch.load(encoder_path, map_location=device)
+
+    #print("Model's state_dict:")
+    #for param_tensor in encoder.state_dict():
+    #    print(param_tensor, "\t", encoder.state_dict()[param_tensor].size())
+    
+    # extract the height and width of image that this model was trained with
+    feed_height = loaded_dict_enc['height']
+    feed_width = loaded_dict_enc['width']
+    filtered_dict_enc = {k: v for k, v in loaded_dict_enc.items() if k in encoder.state_dict()}
+    encoder.load_state_dict(filtered_dict_enc)
+    encoder.to(device)
+    encoder.eval()
+    
+    #summary(encoder, (3,640, 192))
+
+    x = torch.ones((1,3,640,192)).cuda()
+    encoder_trt = torch2trt(encoder, [x])
+
+    torch.save(encoder_trt.state_dict(), 'encoder_trt.pth')
+    
+    print("   Loading pretrained decoder")
+    depth_decoder = networks.DepthDecoder(
+        num_ch_enc=encoder.num_ch_enc, scales=range(4))
+
+    loaded_dict = torch.load(depth_decoder_path, map_location=device)
+    depth_decoder.load_state_dict(loaded_dict)
+
+    depth_decoder.to(device)
+    depth_decoder.eval()
+
+    #print("Model's state_dict:")
+    #for param_tensor in depth_decoder.state_dict():
+    #    print(param_tensor, "\t", depth_decoder.state_dict()[param_tensor].size())
+
+    y = torch.ones((1,512,20,6)).cuda()
+    depth_decoder_trt = torch2trt(depth_decoder, [y])
+
+    vid = cv2.VideoCapture(args.video_path)
+    
+    # FINDING INPUT IMAGES
+    if(args.video_path == '0' or args.video_path == '1'):
+        vid = open_cam_usb(args.video_path, 1920, 1080)
+        print("camera open")
+        output_directory = "./assets"
+        if vid.isOpened() == False: #카메라 생성 확인
+            print("camera can not open")
+            exit()
+    elif os.path.isfile(args.video_path):
+        # Only testing on a single image
+        paths = [args.video_path]
+        output_directory = os.path.dirname(args.video_path)
+    elif os.path.isdir(args.video_path):
+        # Searching folder for images
+        paths = glob.glob(os.path.join(args.video_path, '*.{}'.format(args.ext)))
+        output_directory = args.video_path
+    else:
+        raise Exception("Can not find args.video_path: {}".format(args.video_path))
+    
+    # PREDICTING ON EACH IMAGE IN TURN
+    with torch.no_grad():
+        while(True):
+            ret, frame = vid.read()
+            if(args.video_path != '0' or args.video_path != '1'): 
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            input_image = pil.fromarray(frame)
+            
+            # Load image and preprocess
+            original_width, original_height = input_image.size
+            input_image = input_image.resize((feed_width, feed_height), pil.LANCZOS)
+            input_image = transforms.ToTensor()(input_image).unsqueeze(0)
+
+            prev_time = time.time()
+
+            # PREDICTION
+            input_image = input_image.to(device)
+            features = encoder_trt(input_image)
+            outputs = depth_decoder_trt(features)
+
+            disp = outputs[("disp", 0)]
+            disp_resized = torch.nn.functional.interpolate(
+                disp, (original_height, original_width), mode="bilinear", align_corners=False)
+
+            # Saving numpy file
+            output_name = os.path.splitext(os.path.basename(args.video_path))[0]
+            name_dest_npy = os.path.join(output_directory, "{}_disp.npy".format(output_name))
+            scaled_disp, _ = disp_to_depth(disp, 0.1, 100)
+            np.save(name_dest_npy, scaled_disp.cpu().numpy())
+
+            # Saving colormapped depth image
+            disp_resized_np = disp_resized.squeeze().cpu().numpy()
+            vmax = np.percentile(disp_resized_np, 95)
+            normalizer = mpl.colors.Normalize(vmin=disp_resized_np.min(), vmax=vmax)
+            mapper = cm.ScalarMappable(norm=normalizer, cmap='magma')
+            colormapped_im = (mapper.to_rgba(disp_resized_np)[:, :, :3] * 255).astype(np.uint8)
+            im = pil.fromarray(colormapped_im)
+            
+            curr_time = time.time()
+            exec_time = curr_time - prev_time
+
+            imcv = cv2.cvtColor(np.asarray(im), cv2.COLOR_RGB2BGR)            
+            info = "time:" + str(round(1000*exec_time, 2)) + " ms, FPS: " + str(round((1000/(1000*exec_time)),1))
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            #resize window
+            frame = cv2.resize(frame, dsize=(640,480), interpolation=cv2.INTER_AREA)
+            imcv = cv2.resize(imcv, dsize=(640,480), interpolation=cv2.INTER_AREA)
+            cv2.putText(imcv, text=info, org=(50, 70),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=1, color=(255, 0, 0), thickness=2)
+
+            #show result
+            cv2.imshow("input", frame)
+            cv2.imshow("result",imcv)
+            if cv2.waitKey(10) & 0xFF == ord('q'): break
+
+    print('-> Done!')
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    test_simple(args)
