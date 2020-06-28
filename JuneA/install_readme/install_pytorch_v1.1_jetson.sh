78fa176b9ebe4d6d4c20c9783f94b5079587d574
JuneA <dlwnstjr2004@naver.com>
2020-06-29 00:06:12 +0900

    install_pytorch

--- JuneA/install_readme/install_pytorch_v1.1_jetson.sh
+++ JuneA/install_readme/install_pytorch_v1.1_jetson.sh
@@ -3,7 +3,7 @@ sudo apt-get install python3-pip python3-dev -y
 python3 -m pip install --upgrade pip
 sudo apt-get install libjpeg-dev zlib1g-dev -y
 
-wget https://nvidia.box.com/shared/static/mmu3xb3sp4o8qg9tji90kkxl1eijjfc6.whl –O torch-1.1.0-cp36-cp36m-linux_aarch64.whl
+wget https://nvidia.box.com/shared/static/mmu3xb3sp4o8qg9tji90kkxl1eijjfc6.whl –o torch-1.1.0-cp36-cp36m-linux_aarch64.whl
 
 pip3 install numpy torch-1.1.0-cp36-cp36m-linux_aarch64.whl
 ## torchvision v0.3.0
